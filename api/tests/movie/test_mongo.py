5a544407a9956ba5ec2caab80302b0f1e30243d6
marcin <marcinpiwko7@gmail.com>
2023-01-31 18:47:19 +0100

    Add MongoDB methods

--- api/tests/movie/test_mongo.py
+++ api/tests/movie/test_mongo.py
@@ -7,21 +7,22 @@ from api.repository.movie.mongo import MongoMovieRepository
 @pytest.mark.asyncio
 async def test_create():
     repo = MongoMovieRepository(
-        connection_string="mongodb://localhost:27017",
-        database="my-database"
+        connection_string="mongodb://localhost:27017", database="my-database"
     )
-    await repo.create(movie=Movie(
-        movie_id="first",
-        title="My movie",
-        description="My movie descriptions",
-        release_year=1991
-    ))
+    await repo.create(
+        movie=Movie(
+            movie_id="first",
+            title="My movie",
+            description="My movie descriptions",
+            release_year=1991,
+        )
+    )
     movie = await repo.get("first")
     assert movie == Movie(
         movie_id="first",
         title="My movie",
         description="My movie descriptions",
-        release_year=1991
+        release_year=1991,
     )
     await repo.delete("first")
 
