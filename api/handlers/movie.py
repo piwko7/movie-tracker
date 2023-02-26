49a0e9004a34bf3e8239e412a4af4d63e8b2ecc7
marcin <marcinpiwko7@gmail.com>
2023-02-26 09:19:36 +0100

    Add get endpoint

--- api/handlers/movie.py
+++ api/handlers/movie.py
@@ -7,6 +7,7 @@ from starlette import status
 from api.entities.movie import Movie
 from api.repository.movie.mongo import MongoMovieRepository
 from api.settings import Settings
+from build.lib.api.responses.detail import DetailResponse
 
 router = APIRouter(prefix="/api/v1/movie", tags=["movies"])
 
@@ -30,7 +31,37 @@ async def create_movie(
     repo: MongoMovieRepository = Depends(
         movie_repository,
     ),
-):
+) -> str:
     movie.movie_id = str(uuid.uuid4())
     await repo.create(movie)
     return movie.movie_id
+
+
+@router.get(
+    "/{movie_id}",
+)
+async def get_movie_by_id(
+    movie_id=str,
+    repo: MongoMovieRepository = Depends(
+        movie_repository,
+    ),
+) -> Movie:
+    movie = await repo.get(movie_id=movie_id)
+    if movie is None:
+        return DetailResponse(message=f"Movie with id {movie_id} is not exist")
+    return movie
+
+
+@router.get(
+    "/{title}",
+)
+async def get_movie_by_title(
+    title=str,
+    repo: MongoMovieRepository = Depends(
+        movie_repository,
+    ),
+) -> list[Movie]:
+    movie = await repo.get_by_title(title=title)
+    if movie is None:
+        return DetailResponse(message=f"Movie with title {title} is not exist")
+    return movie
